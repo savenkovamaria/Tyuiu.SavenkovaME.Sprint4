60d58dc067a14b44812a20985d3d6d4f18af491b
Tyuiu.SavenkovaME@outlook.com <savenkova_mars@mail.ru>
2023-10-31 20:33:43 +0500

    Task0

---
+++ Tyuiu.SavenkovaME.Sprint4.Task0.V1.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.SavenkovaME.Sprint4.Task0.V1.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.SavenkovaME.Sprint4.Task0.V1.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a6776a83-2382-4d4e-930f-a476e8811bca")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.SavenkovaME.Sprint4.Task0.V1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.SavenkovaME.Sprint4.Task0.V1.Lib;
+
+namespace Tyuiu.SavenkovaME.Sprint4.Task0.V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #4 | Выполнила: Савенкова М. Е. | ИИПб-23-2";
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* Спринт #4                                                                    *");
+            Console.WriteLine("* Тема: Одномерные массивы (статический ввод)                                  *");
+            Console.WriteLine("* Задание #0                                                                   *");
+            Console.WriteLine("* Вариант #1                                                                   *");
+            Console.WriteLine("* Выполнила Савенкова М. Е. | ИИПб-23-2                                        *");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                     *");
+            Console.WriteLine("* Дан одномерный целочисленный массив на 10 элементов заполненный статическими *");
+            Console.WriteLine("* значениями в диапазоне от 0 до 9 подсчитать сумму четных элементов массива.  *");
+            Console.WriteLine("* {6 ,4 ,3 ,2 ,1 ,0 ,9 ,8 ,7 ,5}                                               *");
+            Console.WriteLine("*                                                                              *");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                             *");
+            Console.WriteLine("********************************************************************************");
+            int[] num = { 6, 4, 3, 2, 1, 0, 9, 8, 7, 5 };
+            int res = ds.GetSumEvenArrEl(num);
+            Console.WriteLine("Исходный массив: ");
+            for (int i = 0; i <= num.Length-1; i++)
+            {
+                Console.WriteLine(num[i]);
+            }
+
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                   *");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("Сумма четных элементов массива = " + res);
+
+            Console.ReadKey();
+        }
+    }
+}
